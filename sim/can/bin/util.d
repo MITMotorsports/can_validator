ee21381187a8de95d07f17e80f548dc9aae56599
Eric Ponce <eaponce@mit.edu>
2016-05-17 12:31:53 -0400

    Added sim stuff

---
+++ sim/can/bin/util.d
@@ -0,0 +1,21 @@
+bin/util.o: ../../evt_lib/src/util.c ../../evt_lib/inc/util.h \
+ ../../lpc_chip_11cxx_lib/inc/lpc_types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20140805/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20140805/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20140805/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20140805/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20140805/lib/gcc/arm-none-eabi/4.8.4/include/stdbool.h
+
+../../evt_lib/inc/util.h:
+
+../../lpc_chip_11cxx_lib/inc/lpc_types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20140805/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20140805/arm-none-eabi/include/stdint.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20140805/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20140805/arm-none-eabi/include/sys/features.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20140805/lib/gcc/arm-none-eabi/4.8.4/include/stdbool.h:
